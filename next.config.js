332557a662885c63959241a4bc931fadce78946f
Oz4 <aowss18@gmail.com>
2022-08-22 16:53:33 +0300

    added: pages on home page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const menu = require("./src/setup/menu.js");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images:{
+  images: {
     domains: ['img.seadn.io'],
-  }
+  },
+  env: {
+    menu: menu.get('src/pages'),
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import { Box } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
@@ -14,10 +15,23 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome
+          4ws
         </h1>
+        <nav>
+          {/* @ts-ignore */}
+          {process?.env?.menu?.map((item) => (
+            <Link key={item.link} href={item.link}>
+              <Box className="text-base text-green-400 cursor-pointer transition-all font-semibold" _hover={{
+                color: '#2DE370',
+                opacity: 0.6
+              }}>
+                {item.name[0].toUpperCase() + item.name.slice(1)}
+              </Box>
+            </Link>
+          ))}
+        </nav>
       </main>
-    </div>
+    </div >
   )
 }
 
