891c6faafa5efd1839fb3081a3b8f03f0176909b
Oz4 <aowss18@gmail.com>
2022-08-31 19:55:11 +0300

    updated: home page font and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,11 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           4ws
         </h1>
-        <nav>
+        <nav className='absolute bottom-0 left-0 flex justify-evenly p-4 w-full overflow-x-auto border-t border-gray-600' id='scroll-bar-green'>
           {/* @ts-ignore */}
-          {process?.env?.menu?.map((item) => (
+          {process?.env?.menu?.filter(item => item.name !== 'Home').map((item) => (
             <Link key={item.link} href={item.link}>
-              <Box className="text-base text-green-400 cursor-pointer transition-all font-semibold" _hover={{
+              <Box className="text-base text-green-400 cursor-pointer transition-all font-semibold mr-2 min-w-[100px]" _hover={{
                 color: '#2DE370',
                 opacity: 0.6
               }}>
