bcd1c8acab8ba8fc2b276ece63e12105beb0d43e
Oz4 <aowss18@gmail.com>
2022-08-31 19:01:09 +0300

    updated: attribute to original page

--- src/pages/vibes.tsx
+++ src/pages/vibes.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Page } from 'src/features/vibes'
 function vibes() {
+
+    const Attribute = () => {
+        return (
+            <div className=' absolute top-[93vh] right-[20px] text-white'>
+                <div className='bg-[#00000077] px-4 py-2 rounded-full text-base font-bold'>
+                    <a href="https://vibes.framer.website/" target="_blank" rel="noopener noreferrer">
+                        Framer Motion
+                        <span className='text-yellow-300 ml-1'>
+                            Vibes
+                        </span>
+                    </a>
+                </div>
+            </div >
+        )
+    }
     return (
         <div>
             <Page />
+            <Attribute />
         </div>
     )
 }
 
 export default vibes
+
